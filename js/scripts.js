5c5e5e1da26d70eb4e33509eda8f438431705064
Konrad Jakubowski <jakubowski.info@gmail.com>
2018-04-13 19:28:58 +0200

    Remove of some whitespace in code

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,9 @@ var newGameBtn = document.getElementById('js-newGameButton');
 
 newGameBtn.addEventListener('click', newGame);
 
-
 var pickRock = document.getElementById('js-playerPick_rock'),
     pickPaper = document.getElementById('js-playerPick_paper'),
     pickScissors = document.getElementById('js-playerPick_scissors');
-
 
 pickRock.addEventListener('click', function() {  
     playerPick('rock') 
